764c88b32709111764e9dc60c0af2df916e25998
Robert Helmer <rhelmer@rhelmer.org>
2015-01-26 20:50:13 -0800

    remove unused files

--- webapp-django/crashstats/auth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from . import views
-
-
-urlpatterns = patterns(
-    '',  # prefix
-    url('^_debug_login/$', views.debug_login, name='debug_login'),
-)
--- webapp-django/crashstats/symbols/tests/base.py
+++
@@ -1,7 +0,0 @@
-import os
-
-_here = os.path.dirname(__file__)
-ZIP_FILE = os.path.join(_here, 'sample.zip')
-TAR_FILE = os.path.join(_here, 'sample.tar')
-TGZ_FILE = os.path.join(_here, 'sample.tgz')
-TARGZ_FILE = os.path.join(_here, 'sample.tar.gz')
